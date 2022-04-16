5196f905ab51dc6078be9c9d27a5701c29093776
kbjdfj00 <taimingzh@163.com>
2022-04-16 23:06:45 +0800

    202204162303_创建项目

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zhangtm3
+ * */
+public class Main {
+    private  static final  Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("main函数日志：????");
+        System.out.println("****");
+        System.out.println("Hello world!");
+    }
+}
